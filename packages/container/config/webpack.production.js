f0d273cefb160a19cc4614b33f94f562aa65e703
kelvin hernandez cabrera <kelhzkb@hotmail.com>
2023-07-25 09:20:02 +0200

    Created container deploy script

---
+++ packages/container/config/webpack.production.js
@@ -0,0 +1,24 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const { PRODUCTION_DOMAIN } = process.env;
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${PRODUCTION_DOMAIN}/marketing/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
