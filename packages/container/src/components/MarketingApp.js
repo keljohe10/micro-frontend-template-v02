b8c9cc4e2ec2c9ab1c37079f78e1ad49036ae4e2
kelvin hernandez cabrera <kelhzkb@hotmail.com>
2023-07-26 16:52:51 +0200

    Deploy v2

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -5,7 +5,6 @@ export default () => {
     const ref = useRef(null);
 
     useEffect(() => {
-        console.log('MarketingApp.js useEffect called');
         MarketingMount(ref.current);
     }, []);
 
