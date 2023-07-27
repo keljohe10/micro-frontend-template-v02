ba9a494ac889d0ad971961825faa55cab62dfd08
kelvin hernandez cabrera <kelhzkb@hotmail.com>
2023-07-27 08:43:23 +0200

    Deploy new marketing App

--- packages/container/config/webpack.production.js
+++ packages/container/config/webpack.production.js
@@ -15,7 +15,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@${PRODUCTION_DOMAIN}/marketing/remoteEntry.js`,
+        marketing: `marketing@${PRODUCTION_DOMAIN}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
